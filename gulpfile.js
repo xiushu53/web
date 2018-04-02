f66db8c9b87bebaa172b241f058aa315acdbe8d0
Yeshi <yoshikema@gmail.com>
2018-04-02 19:01:22 +0900

    とりあえず仮組み

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
 
 var paths = {
   'scss': './src/',
   'css': './dist/'
 }
 
+//CSSプリプロセッサ
 gulp.task("compileCss", function() {
   return gulp.src(paths.scss + "/*.scss")
     .pipe(plumber())
@@ -18,10 +20,25 @@ gulp.task("compileCss", function() {
     .pipe(gulp.dest(paths.css))
 });
 
+//簡易Webサーバー
+gulp.task('webserver', function() {
+  browserSync({
+      server: {
+          baseDir: "./dist/",
+          index: "index.html"
+      }
+  });
+});
+
+//ブラウザリロード
+gulp.task('browser-reload', function(){
+  browserSync.reload();
+})
+
 // CSSファイルの変更を検知してcompileCssを実行
 gulp.task("watch", function () {
-  gulp.watch("src/*.scss", ["compileCss"]);
+  gulp.watch("src/*.scss", ["compileCss","browser-reload"]);
 });
 
 // 実行時にはcompileCssしてwatch
-gulp.task("default", ["compileCss", "watch"]);
+gulp.task("default", ["compileCss", "webserver", "watch"]);
