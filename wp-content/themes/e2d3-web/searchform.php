165a8f200ed93eb97e7567e274bbbed64997a8a2
xiushu53 <xiushu53@gmail.com>
2018-12-21 07:10:56 +0900

    fixed header.php and front-page.php

--- wp-content/themes/e2d3-web/searchform.php
+++
@@ -1,7 +0,0 @@
-<form method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
-  <label>
-		<span class="screen-reader-text">検索:</span>
-		<input type="search" class="search-field" placeholder="検索 …" name="s">
-	</label>
-	<input type="submit" class="search-submit" value="検索">
-</form>
